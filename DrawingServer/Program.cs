06a17f71b697e66ef7c6ef4fdc7b7bfa63adeae0
PhotonicBits <photonicbits@gmail.com>
2017-11-25 10:22:20 +0200

    Add project files.

---
+++ DrawingServer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingServer
+{
+    class Program
+    {
+        static Server server = new Server(5432);
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // add client to server
+                TcpClient client = server.newClient();
+                // connect new client in seperate thread
+                Task.Run(() =>
+                {
+                    Console.WriteLine("user joined");
+                    // while thread is running, send message, else exit loop.
+                    while (true)
+                    {
+                        try
+                        {
+                            string message = server.recieve(client);
+                            Console.WriteLine("recieved "+message);
+                            server.sendToAll(message);
+                        }
+                        catch
+                        {
+                            break;
+                        }
+                    }
+                });
+            }
+            // wait for client
+        }
+    }
+}
---
+++ DrawingServer/Server.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingServer
+{
+    class Server
+    {
+        int port;
+        TcpListener listener;
+        public List<TcpClient> clients = new List<TcpClient>();
+
+        public Server(int port = 5432)
+        {
+            this.port = port;
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+        }
+
+        public void sendToAll(string message)
+        {
+            byte[] bytes = ASCIIEncoding.ASCII.GetBytes(message);
+            for (int x = 0; x < clients.Count; x++)
+            {
+                try
+                {
+                    clients[x].GetStream().Write(bytes, 0, bytes.Length);
+                }
+                catch
+                {
+                    clients.RemoveAt(x);
+                    if (x - 1 < 0)
+                    {
+                        x--;
+                    }
+                }
+            }
+        }
+
+        public TcpClient newClient()
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            clients.Add(client);
+            return client;
+        }
+        /*
+        public void send(string message, TcpClient client)
+        {
+            byte[] bytes = ASCIIEncoding.ASCII.GetBytes(message);
+            client.GetStream().Write(bytes, 0, bytes.Length);
+        }
+        */
+
+        public string recieve(TcpClient client)
+        {
+            byte[] bytes = new byte[client.ReceiveBufferSize];
+            int toRead = client.GetStream().Read(bytes, 0, client.ReceiveBufferSize);
+            return ASCIIEncoding.ASCII.GetString(bytes, 0, toRead);
+        }
+    }
+}
